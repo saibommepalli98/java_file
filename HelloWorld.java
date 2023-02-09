e752f589d616d96525a3df8c26ea94abafeb2986
bommepalli <saibommepalli@gmail.com>
2023-02-09 20:12:15 +0530

    dc

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+         HelloWorld h=new HelloWorld();
+         h.add(100,200);
+         h.sub(10,30);
+         h.mul(1,2);
+         
+    }
+    
+    void add(int a,int b){
+        int c=a+b;
+         System.out.println(c);
+        
+    }
+    
+     void sub(int a,int b){
+        int c=a-b;
+         System.out.println(c);
+        
+    }
+    void mul(int a,int b){
+        int c=a*b;
+         System.out.println(c);
+        
+    }
+}
